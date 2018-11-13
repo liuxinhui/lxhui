45b21deef502a3dc49414ecddb7b58103b7d74e9
liuxinhui <916510655@qq.com>
2018-11-13 11:34:36 +0800

    init

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <home-heaer></home-heaer>
+  </div>
+</template>
+
+<script>
+//把子组件引入到父组件
+import HomeHeaer from './components/Header'
+export default {
+  name: 'Home',
+  //使用引入的子组件
+  components:{
+    HomeHeaer
+  },
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="header">
+        <div class="header-left"><span class="iconfont">&#xe624;</span></div>
+        <div class="header-input"><span class="iconfont">&#xe632;</span>请输入内容</div>
+        <div class="header-right">城市<span class="iconfont">&#xe64a;</span></div>
+    </div>    
+</template>
+<script>
+export default {
+    name:'HomeHeaer',
+}
+</script>
+
+<style lang='stylus' scoped>
+    @import '~styles/varibles.styl'
+    .header
+        display :flex
+        line-height :0.86rem
+        background :$bgColor
+        color :#fff
+        .header-left
+            float :left
+            width :0.64rem
+        .header-input
+            flex :1
+            background :#fff
+            height .64rem
+            margin-top: .12rem 
+            border-radius :0.1rem
+            color :#ccc
+            line-height .64rem
+        .header-right
+            width 1rem;
+            float :right 
+            text-align :center       
+</style>
