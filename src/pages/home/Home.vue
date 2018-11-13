e307e5c54c6a2df85aa614b075ab0f232f3a7d6b
liuxinhui <916510655@qq.com>
2018-11-13 13:58:54 +0800

    swiper

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,17 +1,20 @@
 <template>
   <div>
     <home-heaer></home-heaer>
+    <home-swiper></home-swiper>
   </div>
 </template>
 
 <script>
 //把子组件引入到父组件
 import HomeHeaer from './components/Header'
+import HomeSwiper from './components/swiper'
 export default {
   name: 'Home',
   //使用引入的子组件123
   components:{
-    HomeHeaer
+    HomeHeaer,
+    HomeSwiper
   },
   data () {
     return {
---
+++ src/pages/home/components/swiper.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="swiper">
+        <swiper :options="swiperOption">
+            <swiper-slide><img class="swiper-img" src="https://imgs.qunarzz.com/vc/28/72/3d/07df240399f309da18e9a9854e.png_92.png" /></swiper-slide>
+            <swiper-slide><img class="swiper-img" src="https://imgs.qunarzz.com/vc/42/26/bb/71d41bf5d04fcb589ece936cba.jpg_92.jpg" /></swiper-slide>
+            <div class="swiper-pagination"  slot="pagination"></div>
+        </swiper>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'HomeSwiper',
+    data (){
+        return{
+            swiperOption:{
+                pagination:'swiper-pagination',
+                loop: true
+            }
+        }
+    }
+}
+</script>
+
+<style lang="stylus" scoped>
+    .swiper
+        width :100%
+        height :0
+        overflow :hidden
+        padding-bottom :40%
+        .swiper-img
+            width :100%
+</style>
